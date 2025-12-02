6c2f0c42b11650707c5b27858176e68a24df1d78
erickmerchant <erickmerchant@gmail.com>
2025-12-02 01:57:15 -0500

    Day 2

--- main_test.ts
+++ main_test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "@std/assert";
-import { day1_part1, day1_part2 } from "./main.ts";
+import { day1_part1, day1_part2, day2_part1, day2_part2 } from "./main.ts";
 
 Deno.test(async function day1() {
   const example = `
@@ -17,4 +17,13 @@ Deno.test(async function day1() {
 
   assertEquals(await day1_part1(example), 3);
   assertEquals(await day1_part2(example), 6);
+});
+
+Deno.test(async function day2() {
+  const example = `
+    11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
+  `;
+
+  assertEquals(await day2_part1(example), 1227775554);
+  assertEquals(await day2_part2(example), 4174379265);
 });
