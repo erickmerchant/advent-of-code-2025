e3beef1bd988eb5e04e4b2e189a32f78b19eff6a
erickmerchant <erickmerchant@gmail.com>
2025-12-08 23:50:42 -0500

    Stop manually updating list

--- main.ts
+++ main.ts
@@ -1,22 +1,16 @@
+const IDS: Array<string> = [];
+
+for await (const file of Deno.readDir(".")) {
+  if (file.isDirectory && file.name.startsWith("day")) {
+    IDS.push(file.name + ".part1");
+    IDS.push(file.name + ".part2");
+  }
+}
+
+IDS.sort();
+
 export async function* main(
-  ids: Array<string> = [
-    "day1.part1",
-    "day1.part2",
-    "day2.part1",
-    "day2.part2",
-    "day3.part1",
-    "day3.part2",
-    "day4.part1",
-    "day4.part2",
-    "day5.part1",
-    "day5.part2",
-    "day6.part1",
-    "day6.part2",
-    "day7.part1",
-    "day7.part2",
-    "day8.part1",
-    "day8.part2",
-  ],
+  ids: Array<string> = IDS,
 ): AsyncGenerator<number> {
   const results = [];
 
