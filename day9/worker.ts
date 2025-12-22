fa7b3bcef3acdbe7e881e3342d7a9bbcad65378d
erickmerchant <erickmerchant@gmail.com>
2025-12-22 15:33:03 -0500

    Make day 9 part 2 faster

--- day9/worker.ts
+++ day9/worker.ts
@@ -29,54 +29,42 @@ self.onmessage = (
 
     inner: while (true) {
       for (let x = minX + 1; x < maxX; x++) {
-        const y = minY + 1;
-        const outside = verticals
-              .filter(([x2, y2, y3]) => x2 < x && y > y2 && y < y3)
-              .length % 2 === 0;
+        {
+          const totals = verticals
+            .reduce((acc, [x2, y2, y3]) => [
+              acc[0] + (
+                (x2 < x && (minY + 1) > y2 && (minY + 1) < y3) ? 1 : 0
+              ),
+              acc[1] + (
+                (x2 < x && (maxY - 1) > y2 && (maxY - 1) < y3) ? 1 : 0
+              ),
+            ], [0, 0]);
 
-        if (outside) {
-          out = true;
+          if (totals[0] % 2 === 0 || totals[1] % 2 === 0) {
+            out = true;
 
-          break inner;
-        }
-      }
-
-      for (let x = minX + 1; x < maxX; x++) {
-        const y = maxY - 1;
-        const outside = verticals
-              .filter(([x2, y2, y3]) => x2 < x && y > y2 && y < y3)
-              .length % 2 === 0;
-
-        if (outside) {
-          out = true;
-
-          break inner;
+            break inner;
+          }
         }
       }
 
       for (let y = minY + 1; y < maxY; y++) {
-        const x = minX + 1;
-        const outside = horizontals
-              .filter(([y2, x2, x3]) => y2 < y && x > x2 && x < x3)
-              .length % 2 === 0;
+        {
+          const totals = horizontals
+            .reduce((acc, [y2, x2, x3]) => [
+              acc[0] + (
+                (y2 < y && (minX + 1) > x2 && (minX + 1) < x3) ? 1 : 0
+              ),
+              acc[1] + (
+                (y2 < y && (maxX - 1) > x2 && (maxX - 1) < x3) ? 1 : 0
+              ),
+            ], [0, 0]);
 
-        if (outside) {
-          out = true;
+          if (totals[0] % 2 === 0 || totals[1] % 2 === 0) {
+            out = true;
 
-          break inner;
-        }
-      }
-
-      for (let y = minY + 1; y < maxY; y++) {
-        const x = maxX - 1;
-        const outside = horizontals
-              .filter(([y2, x2, x3]) => y2 < y && x > x2 && x < x3)
-              .length % 2 === 0;
-
-        if (outside) {
-          out = true;
-
-          break inner;
+            break inner;
+          }
         }
       }
 
